b33e52ecd9b8fb6cac2b8053313f506d00329499
ThongP <gbrider1403@gmail.com>
2023-01-07 12:40:58 +0700

    Fixed minor data and prisma schema
    Changed default value and data value in TT_TK table, schema to avoid conflict with topUpAccount function

--- models/employee.model.ts
+++ models/employee.model.ts
@@ -17,6 +17,10 @@ export async function createUser(user) {
   return await prisma.dS_TK.create({data:user});
 }
 
+export async function createUserAccount(user) {
+  return await prisma.tK_TT.create({data:user});
+}
+
 export async function topupAccount(id, amount) {
   return await prisma.tK_TT.update({
     where: {
